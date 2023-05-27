b00bf8bccc287d8b47169391d553ee35471c5bb5
Johnny McGown <jmac3292@gmail.com>
2023-05-27 13:14:53 -0600

    Updated files, added some assest pics

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1 id="about-me">About Me</h1>
+      <article class="my-info">
+            <p>
+                I am a driven worker, focused, and task oriented. I am able to adapt quickly to new styles of
+                work and fill new roles with minimal training. I have a high standard for getting things done in a
+                proper
+                and timely manner. I am a personable individual and enjoy working on teams of like-minded people. I am
+                always up for the next challenge and looking for new opportunities to grow.
+            </p>
+        </article>
+    </div>
+  );
+}
