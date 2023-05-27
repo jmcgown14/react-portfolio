c4b12c176ba23b9bc2d00a90bfd94aa7b55848b8
Johnny McGown <jmac3292@gmail.com>
2023-05-27 12:38:57 -0600

    Start over with activity code as base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-import Home from './pages/Home/Home'
-import Projects from './pages/Projects/Projects'
-import Contact from './pages/Contact/Contact'
-import Nav from './components/Nav/Nav';
-
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Nav />
-        <Routes>
-          <Route exact path ='/' element={<Home />} />
-          <Route exact path ='/projects' element={<Projects />} />
-          <Route exact path ='/contact' element={<Contact />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
