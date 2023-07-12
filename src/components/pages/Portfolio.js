472401b0791e1847b9b41cd7c428197500d10619
Johnny McGown <jmac3292@gmail.com>
2023-07-12 14:08:48 -0600

    updating repos

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -25,9 +25,12 @@ export default function Portfolio() {
           } else if (repository.name === "tech-blog") {
             imageUrl =
               "https://img.freepik.com/free-photo/technology-communication-icons-symbols-concept_53876-120314.jpg?w=996&t=st=1685225217~exp=1685225817~hmac=e0b4963ea1b8054600b014456ed538edb26340908de99cadd54b5e37a48ea1b2";
-          } else if (repository.name === "ecommerce-backend") {
+          } else if (repository.name === "wallstreetcuriosity") {
             imageUrl =
-              "https://img.freepik.com/free-photo/box-market-electronic-ordering-shop-basket_1421-567.jpg?w=1380&t=st=1685225384~exp=1685225984~hmac=22d0ade782da6faef0ec86c5796901e91097d80f2c837501fe79b8a12b6b2bfc";
+              "https://img.freepik.com/free-vector/gradient-stock-market-concept_23-2149166910.jpg?size=626&ext=jpg&ga=GA1.2.1483187716.1685224903&semt=sph";
+          } else if (repository.name === "boardgameshelf") {
+                imageUrl =
+                  "https://img.freepik.com/free-photo/top-view-beautiful-rpg-still-life-items_23-2149282423.jpg?w=1380&t=st=1689192490~exp=1689193090~hmac=7f1a76a6136701d9a73196b399d6f4c72d5174574991c8b9895b86eff423dd4e";
           } else if (repository.name === "react-portfolio") {
             imageUrl =
               "https://img.freepik.com/free-photo/programming-background-collage_23-2149901771.jpg?w=1060&t=st=1685225475~exp=1685226075~hmac=84d5327cd0da2e013ae8393e050c9cdde2c052b523ed4174d6eabb0d3afe2916";
